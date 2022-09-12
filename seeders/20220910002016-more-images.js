caf61edac443f16c5b07e823d4c07d4736bdbeb3
Jeff <j.michael.press@gmail.com>
2022-09-12 15:39:34 -0700

    implementing fix for image pathname

--- seeders/20220910002016-more-images.js
+++ seeders/20220910002016-more-images.js
@@ -4,7 +4,7 @@ module.exports = {
   async up (queryInterface, Sequelize) {
     return queryInterface.bulkInsert('Images', [{
       fileName: '1x01-Get-Away-From-my-Mom-home-movies-10527477-720-480.jpg',
-      imageURL: 'img/content/1x01-Get-Away-From-my-Mom-home-movies-10527477-720-480.jpg',
+      imageURL: '../img/content/1x01-Get-Away-From-my-Mom-home-movies-10527477-720-480.jpg',
       rating: 0,
       uploaderID: 1,
       createdAt: new Date(),
@@ -12,7 +12,7 @@ module.exports = {
     },
     {
       fileName: '616aab0f5a4ec0a0d11c39c3ed622dd5.jpg',
-      imageURL: 'public/img/content/616aab0f5a4ec0a0d11c39c3ed622dd5.jpg',
+      imageURL: '../public/img/content/616aab0f5a4ec0a0d11c39c3ed622dd5.jpg',
       rating: 0,
       uploaderID: 1,
       createdAt: new Date(),
@@ -20,7 +20,7 @@ module.exports = {
     },
     {
       fileName: 'b5a032a8c8e622365ea3374716c9348b.jpg',
-      imageURL: 'public/img/content/b5a032a8c8e622365ea3374716c9348b.jpg',
+      imageURL: '../public/img/content/b5a032a8c8e622365ea3374716c9348b.jpg',
       rating: 0,
       uploaderID: 1,
       createdAt: new Date(),
@@ -28,7 +28,7 @@ module.exports = {
     },
     {
       fileName: 'b6e65897e1d661eda74008d389c21b0e.jpg',
-      imageURL: 'public/img/content/b6e65897e1d661eda74008d389c21b0e.jpg',
+      imageURL: '../public/img/content/b6e65897e1d661eda74008d389c21b0e.jpg',
       rating: 0,
       uploaderID: 1,
       createdAt: new Date(),
@@ -36,7 +36,7 @@ module.exports = {
     },
     {
       fileName: 'th-2156184116.jpeg',
-      imageURL: 'public/img/content/th-2156184116.jpeg',
+      imageURL: '../public/img/content/th-2156184116.jpeg',
       rating: 0,
       uploaderID: 1,
       createdAt: new Date(),

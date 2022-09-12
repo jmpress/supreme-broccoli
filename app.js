dcc2b86e5bff44d3844969b83df7440663c62eb4
Jeff <j.michael.press@gmail.com>
2022-09-12 09:53:07 -0700

    testing more heroku configs

--- app.js
+++ app.js
@@ -54,6 +54,7 @@ app.use(session({
     saveUninitialized: false,
     cookie: { maxAge: 1000*60*60, secure: false, sameSite: 'none' },
     secure: true,  //when in production, make it true.
+    rejectUnauthorized: false,
     store
   })
 )
